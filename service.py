f90842ea92739c46feb4957bef087a72f03a64b9
Melvin Venceslass <mechatmick@gmail.com>
2022-03-02 19:51:14 +0530

    inital commit
    inital commit

---
+++ service.py
@@ -0,0 +1,24 @@
+from optparse import Option
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class CV(BaseModel):
+   name:str
+   age:int
+   dev:bool
+   hobby:Optional[list]
+
+
+
+@app.get("/hello/{my_query}")
+def home(my_query, q:Optional[str]=None):
+    return {"result":"sucess","user_input":my_query,"query":q}
+
+
+@app.put("/endpoint2")
+async def endpoint( resume: CV):
+    "some code db here"
+    return {"username": resume.name}
