d27b9ccf44552b28b7b993585a4da78c3a10b733
Melvin Venceslass <mechatmick@gmail.com>
2022-03-05 11:06:50 +0530

    friday class

--- service.py
+++ service.py
@@ -1,7 +1,10 @@
 from optparse import Option
+from xmlrpc.client import Boolean
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import pybase64
+import time
 
 app = FastAPI()
 
@@ -10,6 +13,14 @@ class CV(BaseModel):
    age:int
    dev:bool
    hobby:Optional[list]
+
+class EMPLOYEE(BaseModel):
+    name:str
+    exp:int
+    yop:int
+    age:int
+    employeed:bool
+
 
 
 
@@ -22,3 +33,42 @@ def home(my_query, q:Optional[str]=None):
 async def endpoint( resume: CV):
     "some code db here"
     return {"username": resume.name}
+
+
+
+
+@app.post("/mypostendpoint")
+async def mpep(emp:EMPLOYEE):
+    return {"emp":emp.name}
+
+
+@app.get("/token")
+async def tokenGenerator():
+
+    timenow = bytes(str(time.time()),"utf-8")
+    token = pybase64.b64encode(timenow, altchars='_:')
+    with open("token.txt","a+") as tokenfile: 
+        tokenfile.write(str(token) + "|\n" )
+        tokenfile.close()
+    return {"token":token}
+
+
+@app.get("/mySecureEndpoint")
+async def msep(token:str,field1:str, field2:str,):
+    
+    file = eval(str(open("token.txt","r+").readlines()))
+    for each in file:
+        if token in each:
+             authorisation = "Welcome Home!"
+             break
+        else:
+             authorisation = "Get Out!"
+             pass
+    
+    return {"server_pass":authorisation}
+     
+
+    
+    
+
+ 
